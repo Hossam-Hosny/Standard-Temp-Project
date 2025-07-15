dcfef3249046c33c5bdce7080ad220f5936aabbd
Hossam Hosny <HossamHosny415@gmail.com>
2025-07-15 15:13:23 +0300

    Initializing EntityFramewrok Package + Setting Up ConnectionString and DbContext

---
+++ Project.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project.Infrastructure.Context;
+
+internal class AppDbContext(DbContextOptions<AppDbContext>options):DbContext(options)
+{
+
+
+}
--- Project.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Project.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Project.Infrastructure.Context;
 
 namespace Project.Infrastructure.Extensions;
 
@@ -7,8 +9,13 @@ public static class ServiceCollectionExtensions
 {
     public static void AddInfrastructure(this IServiceCollection services,IConfiguration config)
     {
+        var connectionString = config.GetConnectionString("LocalConnectionString");
 
-
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString)
+            .EnableSensitiveDataLogging();
+        });
 
 
 
